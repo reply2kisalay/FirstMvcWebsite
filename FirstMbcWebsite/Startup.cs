ad310254eee593db67d2815ff3cf62792626bcb3
reply2kisalay <email2kisalay@gmail.com>
2017-04-15 18:22:44 -0700

    Add project files.

---
+++ FirstMbcWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FirstMbcWebsite.Startup))]
+namespace FirstMbcWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

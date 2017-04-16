dd9db3d749efc4c1a5319eab7ef6090f3aa3e2fd
reply2kisalay <email2kisalay@gmail.com>
2017-04-15 18:57:00 -0700

    Frist Commit

---
+++ FirstMbcWebsite/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FirstMbcWebsite.Models;
+
+namespace FirstMbcWebsite.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            ViewBag.SomeMessage = "This is a message from controller";
+            List<Product> products = new List<Product>();
+            products.Add(new Product() {ProductId=1,ProductName="Logitech"});
+            products.Add(new Product() { ProductId = 2, ProductName = "Apple" });
+            return View(products);
+        }
+    }
+}
